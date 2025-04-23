81fe651369ee8826bb67f64138aeab51569a2d68
maxwell yudanegara <maxwellyudanegara52@gmail.com>
2025-04-23 22:57:18 +0700

    feat: add models game

--- app/Models/Game.php
+++ app/Models/Game.php
@@ -6,5 +6,11 @@ use Illuminate\Database\Eloquent\Model;
 
 class Game extends Model
 {
-    //
+    // Isi yang boleh di‐mass assign
+    protected $fillable = [
+        'nama',
+        'harga',
+        'deskripsi',
+        'gambar',
+    ];
 }
