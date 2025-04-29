c9829da7a665f4a4e90a8eff8b4970c4fd9a4fb8
maxwell yudanegara <maxwellyudanegara52@gmail.com>
2025-04-29 21:05:22 +0700

    Update gamePage.blade.php

--- resources/views/gamePage.blade.php
+++ resources/views/gamePage.blade.php
@@ -42,7 +42,7 @@
         </div>
     </nav>
 
-<div class="container">
+<div class="container py-4">
     <div class="row">
         @foreach ($games as $game)
             <div class="col-12 col-sm-6 col-md-4 col-lg-3 mb-4">
